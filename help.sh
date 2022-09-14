39856c6c1633db5c10bdcade1bed1b9791079b74
Enes <66560757+devenes@users.noreply.github.com>
2022-09-15 01:06:16 +0300

    Create help.sh

---
+++ help.sh
@@ -0,0 +1,20 @@
+# Description: Get examples from kubectl help for a command
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+
+read -p "Enter kubectl command you want to see the examples for
+(example: create deployment): " input
+
+if [ -z "$input" ]; then
+    echo "You must enter a command"
+    exit 1
+else
+    echo -e "${GREEN}Examples for kubectl $input command:"
+    kubectl $input --help | sed -n "/Examples:/,/^[A-Z]/p"  |grep -v "#"
+    if ! [ $? -eq 0 ]; then
+        echo -e "${RED}Error: kubectl $input command not found"
+        exit 1
+    else 
+        exit 0
+    fi
+fi
